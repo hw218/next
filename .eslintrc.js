47777ca9c2c06a4040cb72acee9aca57d2f7e513
何伟 <8958854+hw211@user.noreply.gitee.com>
2022-04-15 08:17:45 +0800

    axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'vue/setup-compiler-macros': true
   },
   extends: [
+    // 设置eslint为vue3最强语法检查
     'plugin:vue/vue3-strongly-recommended',
     'standard'
   ],
@@ -19,11 +20,11 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    // 关闭组建名称必须是多个单词
+    // 关闭组建名称必须是多个单词警告
     'vue/multi-word-component-names': 0
   },
   overrides: [{
-    // 关闭api文件夹接口返回类型不能有_问题。如：login_user命名
+    // 重写api文件夹接口返回类型不能有_问题。如：login_user命名
     files: ['src/api/**/*.ts'],
     rules: { camelcase: 'off' }
   }]
